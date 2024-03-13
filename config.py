39a5cec1a0f464174f7612e654bc91f2fb3c74f2
Dr-Frogster <147110553+Dr-Frogster@users.noreply.github.com>
2024-03-13 16:20:53 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+SECRET_KEY='a unique and long key'
+TITLE='Riki' 
+HISTORY_SHOW_MAX=30
+PIC_BASE = '/static/content/'
+CONTENT_DIR = 'C:/Workspace/CSC440/Riki/Riki/content'
+USER_DIR = 'C:/Workspace/CSC440/Riki/Riki/user'
+NUMBER_OF_HISTORY = 5
+PRIVATE = True
